8ac57b83b86f3fa0c8dceca71e6358a19f78eb06
Tarek Oraby <42799254+tarekoraby@users.noreply.github.com>
2023-09-28 09:26:30 +0300

    Create Todo view

--- frontend/views/todo/TodoView.tsx
+++ frontend/views/todo/TodoView.tsx
@@ -1,9 +1,56 @@
+import Todo from 'Frontend/generated/com/example/application/Todo';
+import TodoModel from "Frontend/generated/com/example/application/TodoModel";
+import { useEffect, useState } from 'react';
+import { useForm } from '@hilla/react-form';
+import { Button } from '@hilla/react-components/Button.js';
+import { Checkbox } from '@hilla/react-components/Checkbox.js';
+import { TextField } from '@hilla/react-components/TextField.js';
+import { TodoEndpoint } from 'Frontend/generated/endpoints';
+
 export default function TodoView() {
-  return (
-    <div className="flex flex-col h-full items-center justify-center p-l text-center box-border">
-      <img style={{ width: '200px' }} src="images/empty-plant.png" />
-      <h2>This place intentionally left empty</h2>
-      <p>It’s a place where you can grow your own UI 🤗</p>
-    </div>
-  );
+    const [todos, setTodos] = useState(Array<Todo>());
+
+    useEffect(() => {
+        TodoEndpoint.findAll().then(setTodos);
+    }, []);
+
+    const { model, field, submit, reset, invalid } = useForm(TodoModel, {
+        onSubmit: async (todo: Todo) => {
+            const saved = await TodoEndpoint.save(todo) ?? todo;
+            setTodos([...todos, saved]);
+            reset();
+        }
+    });
+
+    async function changeStatus(todo: Todo, done: boolean) {
+        const newTodo = { ...todo, done: done };
+        const saved = await TodoEndpoint.save(newTodo) ?? newTodo;
+        setTodos(todos.map(item => item.id === todo.id ? saved : item));
+    }
+
+    return (
+        <>
+            <div className="m-m flex items-baseline gap-m">
+                <TextField
+                    label="Task"
+                    {...field(model.task)}
+                ></TextField>
+                <Button
+                    theme="primary"
+                    disabled={invalid}
+                    onClick={submit}
+                >Add</Button>
+            </div>
+
+            <div className="m-m flex flex-col items-stretch gap-s">
+                {todos.map(todo => (
+                    <Checkbox
+                        key={todo.id}
+                        label={todo.task}
+                        checked={todo.done}
+                        onCheckedChanged={({ detail: { value } }) => changeStatus(todo, value)}/>
+                ))}
+            </div>
+        </>
+    );
 }
