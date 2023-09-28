83ee7f13bb4beb8cae26909d9304d63e35f2f28d
Tarek Oraby <42799254+tarekoraby@users.noreply.github.com>
2023-09-28 09:25:49 +0300

    Add Todo JPA entity and repo

---
+++ src/main/java/com/example/application/Todo.java
@@ -0,0 +1,49 @@
+package com.example.application;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class Todo {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private boolean done = false;
+
+    @NotBlank
+    private String task;
+
+    public Todo() {}
+
+    public Todo(String task) {
+        this.task = task;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+}
